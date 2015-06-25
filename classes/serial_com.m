fb8a7a6801aafc8c76e59bdcaf5cd550f7a6cfd1
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-06-25 14:50:45 -0400

    New class to handle real communication with motor controller
    Init

---
+++ classes/serial_com.m
@@ -0,0 +1,18 @@
+classdef serial_com < handle
+    
+    properties 
+        s=[];
+        
+    end
+    
+    methods
+        
+        function varargout=serial_com(varargin)
+            varargin
+            
+            varargout{1}=0;
+            
+        end
+    end
+    
+end
