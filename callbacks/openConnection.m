4d0f7a53fa486d25d2f5f41baf54f415b4dcfecd
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-06-25 14:49:32 -0400

    Allow dummy connection

--- callbacks/openConnection.m
+++ callbacks/openConnection.m
@@ -1,27 +1,36 @@
-function varargout=openConnection(varargin)
+function openConnection(varargin)
 
 handles=guidata(varargin{1});
-if nargin>=3&&not(isempty(varargin{3}))
-    comport=varargin{3};
-else
-    comport='COM5';
+
+if ispc
+    if nargin>=3&&not(isempty(varargin{3}))
+        comport=varargin{3};
+    else % use default value for 2p PC
+        comport='COM5';
+    end
+    
+    s=serial(comport);
+    s.name='ESP301';
+    s.BaudRate=921600;
+    s.FlowControl='software';
+    s.Terminator='CR/LF';
+    s.Timeout=2;
+    
+    fopen(s);
+    
+    handles.s=s;
+    
+    msg='EX JOYSTICK ON';
+    fprintf(handles.s,msg);
+    
+    set(handles.hEdit01,'String','Connected')
+else % not on pc, not running scim, go in detached mode
+    s.name='detached';
+    s.H=varargin{1};
+    handles.s=s;
+    set(handles.hEdit01,'String','Detached mode')
 end
 
-s=serial(comport);
-s.name='ESP301';
-s.BaudRate=921600;
-s.FlowControl='software';
-s.Terminator='CR/LF';
-s.Timeout=2;
- 
-fopen(s);
-
-handles.s=s;
-
-msg='EX JOYSTICK ON';
-fprintf(handles.s,msg);
-
-set(handles.hEdit01,'String','Connected')
 
 guidata(varargin{1},handles)
 
