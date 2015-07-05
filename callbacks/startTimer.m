bc72ddc58c93f1b6a9c0be12363eb19cbe5d8b03
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-07-05 18:03:29 -0400

    Resetting last_coords to ensure coord box is updated upon timer start

--- callbacks/startTimer.m
+++ callbacks/startTimer.m
@@ -1,6 +1,7 @@
 function startTimer(varargin)
 handles=guidata(varargin{1});
 
-if strcmpi(handles.hTimer.running,'Off')    
+if strcmpi(handles.hTimer.running,'Off')
+    handles.interface.last_coords=[-1 -1 -1];
     start(handles.hTimer)
 end
