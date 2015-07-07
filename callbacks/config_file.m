1ccd39d8328aec805fd8dd2a2ee7b6bf5d47d818
Ben Vermaercke <ben.vermaercke@gmail.com>
2015-07-07 13:42:28 -0400

    Refresh position display after loading calibration file

--- callbacks/config_file.m
+++ callbacks/config_file.m
@@ -3,6 +3,8 @@ function config_file(varargin)
 handles=guidata(varargin{1});
 mode=varargin{3};
 
+interface=handles.interface;
+
 current_folder=pwd;
 %calibration_folder='C:\Users\labuser\Documents\ben\Matlab\GUIs\MotionGUI\Calibrations';
 cd(handles.calibration_folder)
@@ -17,6 +19,10 @@ switch lower(mode)
         loadName=fullfile(pathname,filename);
         load(loadName,'Calibration')
         handles.Calibration=Calibration;
+        
+        interface.do_update=1;
+        interface.update_position=1;
+        handles.interface=interface;
         guidata(handles.hFig,handles)
         update_gui(handles.hFig)
 end
